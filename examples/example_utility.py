2133cb46c542acfe8a3afd8c1959b0f08fe4d153
Vadim Vic <127843214+vadim-vic@users.noreply.github.com>
2025-02-08 23:44:38 +0100

    Add files via upload

---
+++ examples/example_utility.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Set of utility functions
+def u_test(x,y):
+    # This functions needs external tests and usage examples
+    # These tests are unit-tests and statistical tests
+    z = x + y
+    return z
